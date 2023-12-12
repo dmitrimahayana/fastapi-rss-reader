abe2a0152228cacae8118a5aef1e7551a8dc5516
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-12-12 18:58:35 +0700

    update query

--- app/api.py
+++ app/api.py
@@ -13,12 +13,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    return {"service": "/rss/{parameter}",
+            "parameter": ["python", "datascraping", "airflow", "spark", "kafka", "machinelearning"]}
 
-    return {"service": "/rss/{parameter}", "parameter": ["python", "datascraping", "airflow", "spark", "kafka", "machinelearning"]}
 
-
-@app.get("/rss/{skill}")
-def read_item(skill: str):
+@app.get("/rss/upworks-jobs/{skill}")
+def read_item(skill: str, q: Union[str, None] = None, page: int = 10):
     url = ""
     dict_feed = {
         "title": [],
@@ -27,7 +27,7 @@ def read_item(skill: str):
         "published": [],
     }
     if skill == "python":
-        url = "https://www.upwork.com/ab/feed/jobs/rss?location=Americas%2CEurope%2COceania&verified_payment_only=1&q=python&sort=recency&paging=0%3B10&api_params=1&securityToken=d76b00a86ffa60baeb01da154db76acbc3e3fb340eb668f28c222bb46639c8703ab081313822496c9034327bdea763a7d925b795c8689537c032172bc7d66703&userUid=1183197118515154944&orgUid=1183197118523543553"
+        url = f"https://www.upwork.com/ab/feed/jobs/rss?location=Americas%2CEurope%2COceania&verified_payment_only=1&q=python&sort=recency&paging=0%3B{page}&api_params=1&securityToken=d76b00a86ffa60baeb01da154db76acbc3e3fb340eb668f28c222bb46639c8703ab081313822496c9034327bdea763a7d925b795c8689537c032172bc7d66703&userUid=1183197118515154944&orgUid=1183197118523543553"
     elif skill == "datascraping":
         url = "https://www.upwork.com/ab/feed/jobs/rss?location=Americas%2CEurope%2COceania&q=data+scraping&verified_payment_only=1&sort=recency&paging=0%3B10&api_params=1&securityToken=d76b00a86ffa60baeb01da154db76acbc3e3fb340eb668f28c222bb46639c8703ab081313822496c9034327bdea763a7d925b795c8689537c032172bc7d66703&userUid=1183197118515154944&orgUid=1183197118523543553"
     elif skill == "airflow":
